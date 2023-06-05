d34ebca7599f61f8f8f63f0466906b773d99128d
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-06-05 16:21:17 +0600

    Add project files.

---
+++ ifCondition/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifCondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 3;
+
+            if (a+b > 10)
+            {
+                Console.WriteLine("The Answer is greter than 10");
+                
+            }
+            else{
+                Console.WriteLine("The Answer is not greter than 10");
+            }
+        }
+    }
+}
